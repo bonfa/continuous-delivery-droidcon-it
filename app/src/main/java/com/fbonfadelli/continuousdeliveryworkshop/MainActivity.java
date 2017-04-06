4810b3ae888da7b16a59346fca2b1c78292504fe
Francesco Bonfadelli <francesco.bonfadelli@lastminute.com>
2017-04-06 14:30:34 +0200

    add crashlytics

--- app/src/main/java/com/fbonfadelli/continuousdeliveryworkshop/MainActivity.java
+++ app/src/main/java/com/fbonfadelli/continuousdeliveryworkshop/MainActivity.java
@@ -3,10 +3,15 @@ package com.fbonfadelli.continuousdeliveryworkshop;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Fabric.with(this, new Crashlytics());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
