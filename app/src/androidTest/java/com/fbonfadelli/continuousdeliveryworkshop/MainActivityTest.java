893ca43551bab7d685e4582127e250e7bb2f84e6
Francesco Bonfadelli <francesco.bonfadelli@lastminute.com>
2017-04-06 15:05:17 +0200

    add test

---
+++ app/src/androidTest/java/com/fbonfadelli/continuousdeliveryworkshop/MainActivityTest.java
@@ -0,0 +1,24 @@
+package com.fbonfadelli.continuousdeliveryworkshop;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.EspressoException;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void showsHelloWorld() throws Exception {
+        Espresso.onView(withId(R.id.text_view)).check(matches(withText("Hello World!")));
+    }
+}
